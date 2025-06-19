158c05e111178dc2c87cde55041f3aae42465a6a
sasa503exe <sasacomfominha4@gmail.com>
2025-06-18 21:52:44 -0300

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-import React from "https://esm.sh/react@18.2.0";
-import ReactDOM from "https://esm.sh/react-dom@18.2.0/client";
-import App from "./App.js";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(React.createElement(App));
